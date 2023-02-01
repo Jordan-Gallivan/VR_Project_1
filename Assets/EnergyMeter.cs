15e52730d56631a190b245471c5c772b33b0429a
Jordan-Gallivan <jmatt03@gmail.com>
2023-02-01 15:35:46 -0500

    Energy Bar continues

--- Assets/EnergyMeter.cs
+++ Assets/EnergyMeter.cs
@@ -1,3 +1,4 @@
+// using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,19 +8,21 @@ public class EnergyMeter : MonoBehaviour
 
   //Energy Values
   public static float maxEnergy = 100;
-  public static float currEnergy = 100;
-
+  public static float currEnergy = 0;
+  
+  // make method for energy decrements
+  
   // Energy decrementation constants;
-  public Transform small;
-  public Transform medium;
-  public Transform large;
+  public float small = 5.0f;
+  public float medium = 10.0f;
+  public float large = 20.0f;
 
   // Color and Scale Values
   public Color fullEnergyColor = new Color(0.50f, 0.50f, .83f, .69f);
   public Color lowEnergyColor = new Color(1.0f, 0.82f, .26f, .76f);
   public Color emptyEnergyColor = new Color(0.92f, 0.14f, .10f, .69f);
-  public float yScale = .15F;
-  public float zScale = .01F;
+  public float yScale = .15f;
+  public float zScale = .01f;
 
   // Initialize Variables for Energy Meter
   // public GameObject energy = this.gameObject;
@@ -32,21 +35,38 @@ public class EnergyMeter : MonoBehaviour
   {
     // Initialize Full Energy Meter
     this.gameObject.transform.localScale = new Vector3 ((currEnergy / maxEnergy), yScale, zScale);
-    var energyRenderer = this.gameObject.GetComponent<Renderer>();
-    energyRenderer.material.SetColor("_Color", lowEnergyColor);
+    GameObject parent = this.gameObject;
+    GameObject child = parent.transform.GetChild(0).gameObject;
+    var energyRenderer = child.GetComponent<Renderer>();
+    energyRenderer.material.SetColor("_Color", fullEnergyColor);
 
   }
 
   // Update is called once per frame
-  // void Update()
-  // {
-  //     totalTime += Time.deltaTime;
-  //     if (totalTime > .7) {
-  //       currEnergy -= 5;
-        
-  //     }
+  void FixedUpdate()
+  {
+    if (currEnergy < maxEnergy) {
+      currEnergy += .1f;
+      currEnergy = Mathf.Min(currEnergy, maxEnergy);
+    }
+    
+    this.gameObject.transform.localScale = new Vector3 ((currEnergy / maxEnergy), yScale, zScale);
 
-  //     energy.transform.localScale = new Vector3 ((currEnergy / maxEnergy), yScale, zScale);
+
       
-  // }
+      // totalTime += Time.deltaTime;
+      // if (totalTime > .7) {
+      //   currEnergy -= 5;
+        
+      // }
+
+      // energy.transform.localScale = new Vector3 ((currEnergy / maxEnergy), yScale, zScale);
+      
+  }
+  
+  public static void(float energyDecrementer)
+  {
+    currEnergy -= energyDecrementer;
+
+  }
 }
